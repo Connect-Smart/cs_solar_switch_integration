d0926ae1c10966ba0f9bf7d794af6a988556d707
Jos Rothman <jos@rothman.nl>
2023-11-22 11:33:50 +0100

    udpate

--- custom_components/cs_solar_switch/const.py
+++ custom_components/cs_solar_switch/const.py
@@ -1,4 +1,2 @@
-DEFAULT_NAME = "CS solar switch"
 DOMAIN = "cs_solar_switch"
-VERSION = "1.0.1"
 PLATFORMS = ["switch"]
--- custom_components/cs_solar_switch/switch.py
+++ custom_components/cs_solar_switch/switch.py
@@ -1,16 +1,19 @@
 import logging
+import requests
 from homeassistant.helpers.entity import ToggleEntity
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+#API_URL = "https://api.connect-smart.nl/cs_solar_switch"
+API_URL = "https://www.voxip.nl/api/"
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the CS Solar Switch."""
-    async_add_entities([CssolarSwitch()])
+    async_add_entities([CSSolarSwitch()])
 
 
-class CssolarSwitch(ToggleEntity):
+class CSSolarSwitch(ToggleEntity):
     """Representation of a CS Solar Switch."""
 
     def __init__(self):
@@ -29,10 +32,23 @@ class CssolarSwitch(ToggleEntity):
 
     async def async_turn_on(self, **kwargs):
         """Turn the switch on."""
-        self._state = True
+        # Implement logic to update the switch state from the external API
+        self._update_state()
         self.schedule_update_ha_state()
 
     async def async_turn_off(self, **kwargs):
         """Turn the switch off."""
-        self._state = False
+        # Implement logic to update the switch state from the external API
+        self._update_state()
         self.schedule_update_ha_state()
+
+    def _update_state(self):
+        """Update the state from the external API."""
+        try:
+            response = requests.get(API_URL)
+            response.raise_for_status()
+            data = response.json()
+            self._state = data["switch_state"]
+        except requests.exceptions.RequestException as err:
+            _LOGGER.error(f"Error updating state: {err}")
+            self._state = False  # Set to False in case of an error
