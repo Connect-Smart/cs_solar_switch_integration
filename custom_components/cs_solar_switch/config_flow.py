bf421a68c036a33de4073925a876ffb76528e7d5
Jos Rothman <jos@rothman.nl>
2023-11-22 12:11:24 +0100

    udpate

--- custom_components/cs_solar_switch/config_flow.py
+++ custom_components/cs_solar_switch/config_flow.py
@@ -7,13 +7,21 @@ DOMAIN = "cs_solar_switch"
 async def async_setup(hass, config):
     return True
 
+class CsSolarSwitchFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Cs Solar Switch",
+                data={},
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=config_entries.HANDLER_SCHEMA,
+        )
+
 config_entry_flow.register_discovery_flow(
     DOMAIN,
     "Cs Solar Switch",
-    lambda _: config_entries.ConfigEntry(
-        version=1,
-        domain=DOMAIN,
-        title="Cs Solar Switch",
-        data={},
-    )
+    CsSolarSwitchFlowHandler,
 )
